adab9408a3f8dfb4bffd466634e48601b99b8410
karolh95 <karolh95@live.com>
2020-06-16 11:10:59 +0200

    First commit

---
+++ src/main/java/karolh95/classicmodels/ClassicModelsApplication.java
@@ -0,0 +1,13 @@
+package karolh95.classicmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicModelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/karolh95/classicmodels/ClassicModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package karolh95.classicmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
