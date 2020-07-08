1edd34b05258110986bf9f8f6920386f86ad3b15
karolh95 <karolh95@live.com>
2020-07-08 14:07:21 +0200

    Enhanced tests for Productline Mapper

--- src/test/java/karolh95/classicmodels/utils/ProductlineUtil.java
+++ src/test/java/karolh95/classicmodels/utils/ProductlineUtil.java
@@ -3,6 +3,9 @@ package karolh95.classicmodels.utils;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 
 import karolh95.classicmodels.dto.DtoProductline;
@@ -22,6 +25,30 @@ public class ProductlineUtil {
 		return productline;
 	}
 
+	public static DtoProductline dtoProductline() {
+
+		DtoProductline dtoProductline = new DtoProductline();
+
+		dtoProductline.setProductline("productline");
+		dtoProductline.setTextDescription("textDescription");
+		dtoProductline.setHtmlDescription("htmlDescription");
+		dtoProductline.setImage(new byte[1]);
+
+		return dtoProductline;
+	}
+
+	public static List<Productline> productlines() {
+
+		Productline productline = productline();
+		List<Productline> productlines = new ArrayList<>(3);
+
+		productlines.add(productline);
+		productlines.add(productline);
+		productlines.add(productline);
+
+		return productlines;
+	}
+
 	public static void assertEquals(Productline productline, DtoProductline dtoProductline) {
 
 		assertNotNull(productline, "Productline should not be null");
@@ -34,5 +61,15 @@ public class ProductlineUtil {
 		Assertions.assertEquals(productline.getHtmlDescription(), dtoProductline.getHtmlDescription(),
 				"Html description should match");
 		assertArrayEquals(productline.getImage(), dtoProductline.getImage(), "Image should match");
+	}
+
+	public static void assertUpdated(Productline original, DtoProductline dtoProductline, Productline productline) {
+
+		Assertions.assertEquals(original.getProductline(), productline.getProductline(), "Productline should match");
+		Assertions.assertEquals(dtoProductline.getHtmlDescription(), productline.getHtmlDescription(),
+				"HtmlDescription should match");
+		Assertions.assertEquals(dtoProductline.getTextDescription(), productline.getTextDescription(),
+				"TextDescription should match");
+		assertArrayEquals(dtoProductline.getImage(), productline.getImage(), "Image should match");
 	}
 }
