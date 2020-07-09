84497264cfef9a8afcb700b13810a77c8a02875f
karolh95 <karolh95@live.com>
2020-07-09 14:25:51 +0200

    Enhanced tests for Office Mapper

--- src/test/java/karolh95/classicmodels/mappers/OfficeMapperTests.java
+++ src/test/java/karolh95/classicmodels/mappers/OfficeMapperTests.java
@@ -1,53 +1,176 @@
 package karolh95.classicmodels.mappers;
 
+import static karolh95.classicmodels.utils.OfficeUtil.assertEquals;
+import static karolh95.classicmodels.utils.OfficeUtil.assertUpdated;
+import static karolh95.classicmodels.utils.OfficeUtil.assertUpdatedWithoutAddress;
+import static karolh95.classicmodels.utils.OfficeUtil.office;
+import static karolh95.classicmodels.utils.OfficeUtil.offices;
+import static karolh95.classicmodels.utils.OfficeUtil.dtoOffice;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Optional;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
-import karolh95.classicmodels.dto.DtoAddress;
 import karolh95.classicmodels.dto.DtoOffice;
+import karolh95.classicmodels.mapper.AddressMapper;
+import karolh95.classicmodels.mapper.AddressMapperImpl;
 import karolh95.classicmodels.mapper.OfficeMapper;
-import karolh95.classicmodels.model.Address;
+import karolh95.classicmodels.mapper.OfficeMapperImpl;
 import karolh95.classicmodels.model.Office;
-import karolh95.classicmodels.repository.OfficeRepository;
 
-@SpringBootTest
-public class OfficeMapperTests {
+class OfficeMapperTests {
 
-	@Autowired
-	private OfficeMapper mapper;
+	@Spy
+	AddressMapper addressMapper = new AddressMapperImpl();
 
-	@Autowired
-	private OfficeRepository repository;
+	@InjectMocks
+	OfficeMapper mapper = new OfficeMapperImpl();
 
-	@Test
-	@DisplayName("Should map Office to Dto")
-	public void mapOfficeToDto() {
+	@BeforeEach
+	void init() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-		Optional<Office> optional = this.repository.findById("1");
+	@Nested
+	@DisplayName("officeToDto test")
+	class OfficeToDtoTest {
 
-		assertTrue(optional.isPresent(), "Office should exist");
+		@Test
+		@DisplayName("Should not map null Office object to dto")
+		void mapNullOfficeToDto() {
 
-		Office office = optional.get();
-		Address address = office.getAddress();
-		DtoOffice dto = this.mapper.officeToDto(office);
-		DtoAddress dtoAddress = dto.getAddress();
+			Office office = null;
 
-		assertEquals(office.getOfficeCode(), dto.getOfficeCode(), "Office code should match");
-		assertEquals(office.getPostalCode(), dto.getPostalCode(), "Postal code should match");
-		assertEquals(office.getTerritory(), dto.getTerritory(), "Territory should match");
+			DtoOffice dtoOffice = mapper.officeToDto(office);
 
-		assertEquals(address.getAddressLine1(), dtoAddress.getAddressLine1(), "Address line 1 should match");
-		assertEquals(address.getAddressLine2(), dtoAddress.getAddressLine2(), "Address line 2 should match");
-		assertEquals(address.getCity(), dtoAddress.getCity(), "City should match");
-		assertEquals(address.getCountry(), dtoAddress.getCountry(), "Country should match");
-		assertEquals(address.getPhone(), dtoAddress.getPhone(), "Phone should match");
-		assertEquals(address.getState(), dtoAddress.getState(), "State should match");
+			assertNull(dtoOffice, "Dto Office should be null");
+		}
+
+		@Test
+		@DisplayName("Should map Office to Dto")
+		void mapOfficeToDto() {
+
+			Office office = office();
+			DtoOffice dtoOffice = mapper.officeToDto(office);
+
+			assertEquals(office, dtoOffice);
+		}
+	}
+
+	@Nested
+	@DisplayName("officesToDtos test")
+	class OfficesToDtosTest {
+
+		List<Office> offices;
+		List<DtoOffice> dtoOffices;
+
+		@BeforeEach
+		void init() {
+			offices = offices();
+		}
+
+		@Test
+		@DisplayName("Should not map null offices to dtos")
+		void mapNullOfficesToDtos() {
+
+			offices = null;
+
+			dtoOffices = mapper.officesToDtos(offices);
+
+			assertNull(dtoOffices, "Dto offices should be null");
+		}
+
+		@Test
+		@DisplayName("Should map empty offices to dtos")
+		void mapEmptyOfficesToDtos() {
+
+			offices.clear();
+
+			dtoOffices = mapper.officesToDtos(offices);
+
+			assertTrue(dtoOffices.isEmpty(), "Dto offices should be empty");
+		}
+
+		@Test
+		@DisplayName("Should map offices to dtos")
+		void mapOfficesToDtos() {
+
+			dtoOffices = mapper.officesToDtos(offices);
+
+			for (int i = 0; i < offices.size(); i++) {
+
+				Office office = offices.get(i);
+				DtoOffice dtoOffice = dtoOffices.get(i);
+
+				assertEquals(office, dtoOffice);
+			}
+
+		}
+	}
+
+	@Nested
+	@DisplayName("updateFromDto test")
+	class UpdateFromDtoTest {
+
+		final Office original = office();
+		Office office;
+		DtoOffice dtoOffice;
+
+		@BeforeEach
+		void init() {
+			office = office();
+			dtoOffice = dtoOffice();
+		}
+
+		@Test
+		@DisplayName("Should not update office from null dto")
+		void updateFromNullDto() {
+
+			dtoOffice = null;
+
+			mapper.updateFromDto(dtoOffice, office);
+
+			assertEquals(original, office, "Should not update office");
+		}
+
+		@Test
+		@DisplayName("Should update office from dto without address")
+		void updateOfficeFromDtoWithoutAddress() {
+
+			dtoOffice.setAddress(null);
+
+			mapper.updateFromDto(dtoOffice, office);
+
+			assertUpdatedWithoutAddress(original, dtoOffice, office);
+		}
+
+		@Test
+		@DisplayName("Should update office without address from dto")
+		void updateOfficeWithoutAddressToDto() {
+
+			office.setAddress(null);
+
+			mapper.updateFromDto(dtoOffice, office);
+
+			assertUpdated(original, dtoOffice, office);
+		}
+
+		@Test
+		@DisplayName("Should update office from dto")
+		void updateOfficeFromDto() {
+
+			mapper.updateFromDto(dtoOffice, office);
+
+			assertUpdated(original, dtoOffice, office);
+		}
 	}
 }
--- src/test/java/karolh95/classicmodels/utils/OfficeUtil.java
+++ src/test/java/karolh95/classicmodels/utils/OfficeUtil.java
@@ -2,6 +2,9 @@ package karolh95.classicmodels.utils;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 
 import karolh95.classicmodels.dto.DtoOffice;
@@ -22,6 +25,29 @@ public class OfficeUtil {
 		return office;
 	}
 
+	public static DtoOffice dtoOffice() {
+
+		DtoOffice dtoOffice = new DtoOffice();
+
+		dtoOffice.setOfficeCode("2");
+		dtoOffice.setPostalCode("new_postalCode");
+		dtoOffice.setTerritory("new_territory");
+		dtoOffice.setAddress(AddressUtil.dtoAddress());
+
+		return dtoOffice;
+	}
+
+	public static List<Office> offices() {
+
+		List<Office> offices = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			offices.add(office());
+		}
+
+		return offices;
+	}
+
 	public static void assertEquals(Office office, DtoOffice dtoOffice) {
 
 		assertNotNull(office, "Office should not be null");
@@ -32,5 +58,20 @@ public class OfficeUtil {
 		Assertions.assertEquals(office.getTerritory(), dtoOffice.getTerritory(), "Territory should match");
 
 		AddressUtil.assertEquals(office.getAddress(), dtoOffice.getAddress());
+	}
+
+	public static void assertUpdated(Office original, DtoOffice dtoOffice, Office office) {
+
+		assertUpdatedWithoutAddress(original, dtoOffice, office);
+
+		AddressUtil.assertEquals(office.getAddress(), dtoOffice.getAddress());
+	}
+
+	public static void assertUpdatedWithoutAddress(Office original, DtoOffice dtoOffice, Office office) {
+		Assertions.assertEquals(original.getOfficeCode(), office.getOfficeCode(), "Office code should match");
+
+		Assertions.assertEquals(office.getPostalCode(), dtoOffice.getPostalCode(), "Postal code should match");
+		Assertions.assertEquals(office.getTerritory(), dtoOffice.getTerritory(), "Territory should match");
+
 	}
 }
