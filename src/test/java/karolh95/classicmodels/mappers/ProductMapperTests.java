83c859041d1530384fe492cc4986d8cbf63f387c
karolh95 <karolh95@live.com>
2020-07-10 12:55:39 +0200

    Enhanced tests for Product Mapper

--- src/test/java/karolh95/classicmodels/mappers/ProductMapperTests.java
+++ src/test/java/karolh95/classicmodels/mappers/ProductMapperTests.java
@@ -1,47 +1,172 @@
 package karolh95.classicmodels.mappers;
 
+import static karolh95.classicmodels.utils.ProductUtil.assertEquals;
+import static karolh95.classicmodels.utils.ProductUtil.assertEqualsWithoutProductline;
+import static karolh95.classicmodels.utils.ProductUtil.assertUpdated;
+import static karolh95.classicmodels.utils.ProductUtil.dtoProduct;
+import static karolh95.classicmodels.utils.ProductUtil.product;
+import static karolh95.classicmodels.utils.ProductUtil.products;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
-import java.util.Optional;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import karolh95.classicmodels.dto.DtoProduct;
 import karolh95.classicmodels.mapper.ProductMapper;
+import karolh95.classicmodels.mapper.ProductMapperImpl;
+import karolh95.classicmodels.mapper.resolver.ProductlineResolver;
 import karolh95.classicmodels.model.Product;
-import karolh95.classicmodels.repository.ProductRepository;
+import karolh95.classicmodels.utils.ProductlineUtil;
 
-@SpringBootTest
-public class ProductMapperTests {
-	
-	@Autowired
-	private ProductMapper mapper;
+class ProductMapperTests {
 
-	@Autowired
-	private ProductRepository repository;
+	@Mock
+	ProductlineResolver resolver;
 
-	@Test
-	@DisplayName("Should map Product to Dto")
-	public void mapProductToDto(){
+	@InjectMocks
+	ProductMapper mapper = new ProductMapperImpl();
 
-		Optional<Product> optional = this.repository.findById("S10_1678");
+	@Nested
+	class ProductToDtoTest {
 
-		assertTrue(optional.isPresent(), "Product should exist");
+		Product product;
 
-		Product product = optional.get();
-		DtoProduct dto = this.mapper.productToDto(product);
+		@BeforeEach
+		void init() {
 
-		assertEquals(product.getProductCode(), dto.getProductCode(), "Product code should match");
-		assertEquals(product.getProductName(), dto.getProductName(), "Product name should match");
-		assertEquals(product.getProductScale(), dto.getProductScale(), "Product scale should match");
-		assertEquals(product.getProductVendor(), dto.getProductVendor(), "Product vendor should match");
-		assertEquals(product.getProductDescription(), dto.getProductDescription(), "Product description should match");
-		assertEquals(product.getQuantityInStock(), dto.getQuantityInStock(), "Quantity in stock should match");
-		assertEquals(product.getMSRP(), dto.getMSRP(), "MSRP should match");
-		assertEquals(product.getProductline().getProductline(), dto.getProductline(), "Productline should match");
+			product = product();
+		}
+
+		@Test
+		@DisplayName("Should not map null object")
+		void mapNullProductToDto() {
+
+			product = null;
+
+			DtoProduct dtoProduct = mapper.productToDto(product);
+
+			assertNull(dtoProduct, "DtoProduct should be null");
+		}
+
+		@Test
+		@DisplayName("Should map Product without Productline to dto")
+		void mapProductWithoutProductlineToDto() {
+
+			product.setProductline(null);
+
+			DtoProduct dtoProduct = mapper.productToDto(product);
+
+			assertEqualsWithoutProductline(product, dtoProduct);
+		}
+
+		@Test
+		@DisplayName("Should map Product to Dto")
+		void mapProductToDto() {
+
+			Product product = product();
+
+			DtoProduct dtoProduct = mapper.productToDto(product);
+
+			assertEquals(product, dtoProduct);
+		}
 	}
+
+	@Nested
+	class ProductsToDtosTest {
+
+		List<Product> products;
+		List<DtoProduct> dtoProducts;
+
+		@BeforeEach
+		void init() {
+			products = products();
+		}
+
+		@Test
+		@DisplayName("Should not map null product list to dtos")
+		void mapNullProductsToDtos() {
+
+			products = null;
+
+			dtoProducts = mapper.productsToDtos(products);
+
+			assertNull(dtoProducts, "Dto product list should be null");
+		}
+
+		@Test
+		@DisplayName("Should map empty product list to dtos")
+		void mapEmptyProductsToDtos() {
+
+			products.clear();
+
+			dtoProducts = mapper.productsToDtos(products);
+
+			assertTrue(dtoProducts.isEmpty(), "Dto product list should not contain elements");
+		}
+
+		@Test
+		@DisplayName("Should map Product list to dtos")
+		void mapProductsToDtos() {
+
+			dtoProducts = mapper.productsToDtos(products);
+
+			for (int i = 0; i < products.size(); i++) {
+
+				Product product = products.get(i);
+				DtoProduct dtoProduct = dtoProducts.get(i);
+
+				assertEquals(product, dtoProduct);
+			}
+		}
+	}
+
+	@Nested
+	class UpdateFromDtoTest {
+
+		final Product original = product();
+		Product product;
+		DtoProduct dtoProduct;
+
+		@BeforeEach
+		void init() {
+
+			MockitoAnnotations.initMocks(ProductMapperTests.this);
+			product = product();
+			dtoProduct = dtoProduct();
+		}
+
+		@Test
+		@DisplayName("Should not update Product when Dto is null")
+		void updateProductWithNullDto() {
+
+			dtoProduct = null;
+
+			mapper.updateFromDto(dtoProduct, product);
+
+			assertEquals(original, product, "Product should not be updated");
+		}
+
+		@Test
+		@DisplayName("Should update Product from Dto")
+		void updateFromDto() {
+
+			when(resolver.map(anyString())).thenReturn(ProductlineUtil.productline());
+
+			mapper.updateFromDto(dtoProduct, product);
+
+			assertUpdated(original, dtoProduct, product);
+		}
+	}
+
 }
--- src/test/java/karolh95/classicmodels/utils/ProductUtil.java
+++ src/test/java/karolh95/classicmodels/utils/ProductUtil.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static karolh95.classicmodels.utils.ProductlineUtil.productline;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 
@@ -31,7 +33,44 @@ public class ProductUtil {
 		return product;
 	}
 
+	public static List<Product> products() {
+
+		List<Product> products = new ArrayList<>();
+
+		for (int i = 0; i < 3; i++) {
+			products.add(product());
+		}
+
+		return products;
+	}
+
+	public static DtoProduct dtoProduct() {
+
+		DtoProduct dtoProduct = new DtoProduct();
+
+		dtoProduct.setProductCode("productCode");
+		dtoProduct.setProductName("productName");
+		dtoProduct.setProductScale("productScale");
+		dtoProduct.setProductVendor("productVendor");
+		dtoProduct.setProductDescription("productDescription");
+		dtoProduct.setQuantityInStock((short) 0);
+		dtoProduct.setBuyPrice(new BigDecimal("1.00"));
+		dtoProduct.setMSRP(new BigDecimal("0.25"));
+
+		return dtoProduct;
+	}
+
 	public static void assertEquals(Product product, DtoProduct dtoProduct) {
+
+		assertEqualsWithoutProductline(product, dtoProduct);
+
+		Productline productline = product.getProductline();
+
+		assertNotNull(productline, "Productline shoud not be null");
+		Assertions.assertEquals(productline.getProductline(), dtoProduct.getProductline(), "Productline should match");
+	}
+
+	public static void assertEqualsWithoutProductline(Product product, DtoProduct dtoProduct) {
 
 		assertNotNull(product, "Product should not be null");
 		assertNotNull(dtoProduct, "DTO Product should not be null");
@@ -46,10 +85,21 @@ public class ProductUtil {
 		Assertions.assertEquals(product.getQuantityInStock(), dtoProduct.getQuantityInStock(),
 				"Quantity in stock should match");
 		Assertions.assertEquals(product.getMSRP(), dtoProduct.getMSRP(), "MSRP should match");
+	}
 
-		Productline productline = product.getProductline();
+	public static void assertUpdated(Product original, DtoProduct dtoProduct, Product product) {
 
-		assertNotNull(productline, "Productline shoud not be null");
-		Assertions.assertEquals(productline.getProductline(), dtoProduct.getProductline(), "Productline should match");
+		Assertions.assertEquals(original.getProductCode(), product.getProductCode(), "Product code should match");
+
+		Assertions.assertEquals(dtoProduct.getProductName(), product.getProductName(), "Product name should match");
+		Assertions.assertEquals(dtoProduct.getProductScale(), product.getProductScale(), "Product scale should match");
+		Assertions.assertEquals(dtoProduct.getProductVendor(), product.getProductVendor(),
+				"Product vendor should match");
+		Assertions.assertEquals(dtoProduct.getProductDescription(), product.getProductDescription(),
+				"Product description should match");
+		Assertions.assertEquals(dtoProduct.getQuantityInStock(), product.getQuantityInStock(),
+				"Quantity in stock should match");
+		Assertions.assertEquals(dtoProduct.getBuyPrice(), product.getBuyPrice(), "Buy price should match");
+		Assertions.assertEquals(dtoProduct.getMSRP(), product.getMSRP(), "MSRP should match");
 	}
 }
