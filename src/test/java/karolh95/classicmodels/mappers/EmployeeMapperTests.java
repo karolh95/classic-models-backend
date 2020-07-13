40b7372bb9c372abbc109a6991af265d6a08deb9
karolh95 <karolh95@live.com>
2020-07-13 12:53:23 +0200

    Enhanced tests for Employee Mapper

--- src/test/java/karolh95/classicmodels/mappers/EmployeeMapperTests.java
+++ src/test/java/karolh95/classicmodels/mappers/EmployeeMapperTests.java
@@ -1,15 +1,23 @@
 package karolh95.classicmodels.mappers;
 
+import static karolh95.classicmodels.utils.EmployeeUtil.assertEquals;
+import static karolh95.classicmodels.utils.EmployeeUtil.assertUpdated;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 import karolh95.classicmodels.dto.DtoEmployee;
 import karolh95.classicmodels.mapper.EmployeeMapper;
@@ -17,48 +25,134 @@ import karolh95.classicmodels.mapper.EmployeeMapperImpl;
 import karolh95.classicmodels.mapper.resolver.EmployeeResolver;
 import karolh95.classicmodels.mapper.resolver.OfficeResolver;
 import karolh95.classicmodels.model.Employee;
+import karolh95.classicmodels.utils.EmployeeUtil;
+import karolh95.classicmodels.utils.OfficeUtil;
 
 public class EmployeeMapperTests {
 
-	@Spy
+	@Mock
 	private EmployeeResolver employeeResolver;
 
-	@Spy
+	@Mock
 	private OfficeResolver officeResolver;
 
 	@InjectMocks
 	private EmployeeMapper mapper = new EmployeeMapperImpl();
 
-	@Test
-	@DisplayName("Should map Employee to Dto")
-	public void mapEmployeeToDto() {
+	@Nested
+	@DisplayName("employeeToDto tests")
+	class EmployeeToDtoTests {
 
-		MockitoAnnotations.initMocks(this);
-		doReturn(null).when(employeeResolver).map(anyLong());
-		doReturn(null).when(officeResolver).map(anyString());
-		Employee employee = employee();
-		
-		DtoEmployee dto = mapper.employeeToDto(employee);
+		@Test
+		@DisplayName("Should not map null Employee")
+		void mapNullEmployeeToDto() {
 
-		assertEquals(employee.getEmployeeNumber(), dto.getEmployeeNumber(), "Employee number should match");
-		assertEquals(employee.getLastName(), dto.getLastName(), "Last name should match");
-		assertEquals(employee.getFirstName(), dto.getFirstName(), "First name should match");
-		assertEquals(employee.getExtension(), dto.getExtension(), "Extension shouold match");
-		assertEquals(employee.getEmail(), dto.getEmail(), "Email should match");
-		assertEquals(employee.getJobTitle(), dto.getJobTitle(), "Job title should match");
+			Employee employee = null;
+
+			DtoEmployee dtoEmployee = mapper.employeeToDto(employee);
+
+			assertNull(dtoEmployee, "Dto employee should be null");
+		}
+
+		@Test
+		@DisplayName("Should map Employee to Dto")
+		public void mapEmployeeToDto() {
+
+			Employee employee = EmployeeUtil.employee();
+
+			DtoEmployee dtoEmployee = mapper.employeeToDto(employee);
+
+			assertEquals(employee, dtoEmployee);
+		}
 	}
 
-	private Employee employee() {
+	@Nested
+	@DisplayName("employeesToDtos tests")
+	class EmployeesToDtosTests {
 
-		Employee employee = new Employee();
+		List<Employee> employees;
 
-		employee.setEmployeeNumber(1L);
-		employee.setLastName("lastName");
-		employee.setFirstName("firstName");
-		employee.setExtension("extension");
-		employee.setEmail("email");
-		employee.setJobTitle("jobTitle");
+		@BeforeEach
+		void init() {
+			employees = EmployeeUtil.employees();
+		}
 
-		return employee;
+		@Test
+		@DisplayName("Should not map null employees")
+		void mapNullEmployeesToDtos() {
+
+			employees = null;
+
+			List<DtoEmployee> dtoEmployees = mapper.employeesToDtos(employees);
+
+			assertNull(dtoEmployees, "Dto employees should be null");
+		}
+
+		@Test
+		@DisplayName("Should map empty employees")
+		void mapEmptyEmployeesToDtos() {
+
+			employees.clear();
+
+			List<DtoEmployee> dtoEmployees = mapper.employeesToDtos(employees);
+
+			assertTrue(dtoEmployees.isEmpty(), "Dto employees should be empty");
+		}
+
+		@Test
+		@DisplayName("Should map employees")
+		void mapEmployeesToDtos() {
+
+			List<DtoEmployee> dtoEmployees = mapper.employeesToDtos(employees);
+
+			for (int i = 0; i < employees.size(); i++) {
+
+				Employee employee = employees.get(i);
+				DtoEmployee dtoEmployee = dtoEmployees.get(i);
+
+				assertEquals(employee, dtoEmployee);
+			}
+		}
+	}
+
+	@Nested
+	@DisplayName("updateFromDto tests")
+	class UpdateFromDtoTests {
+
+		final Employee original = EmployeeUtil.employee();
+		Employee employee;
+		DtoEmployee dtoEmployee;
+
+		@BeforeEach
+		void init() {
+
+			MockitoAnnotations.initMocks(EmployeeMapperTests.this);
+
+			doReturn(EmployeeUtil.employee()).when(employeeResolver).map(anyLong());
+			doReturn(OfficeUtil.office()).when(officeResolver).map(anyString());
+
+			employee = EmployeeUtil.employee();
+			dtoEmployee = EmployeeUtil.dtoNewEmployee();
+		}
+
+		@Test
+		@DisplayName("Should not update Employee from null dto")
+		void updateEmployeeFromNullDto() {
+
+			dtoEmployee = null;
+
+			mapper.updateFromDto(dtoEmployee, employee);
+
+			assertEquals(original, employee, "Employee should not change");
+		}
+
+		@Test
+		@DisplayName("Should update Employee from dto")
+		void updateEmployeeFromDto() {
+
+			mapper.updateFromDto(dtoEmployee, employee);
+
+			assertUpdated(original, dtoEmployee, employee);
+		}
 	}
 }
--- src/test/java/karolh95/classicmodels/utils/EmployeeUtil.java
+++ src/test/java/karolh95/classicmodels/utils/EmployeeUtil.java
@@ -2,6 +2,9 @@ package karolh95.classicmodels.utils;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 
 import karolh95.classicmodels.dto.DtoEmployee;
@@ -26,6 +29,31 @@ public class EmployeeUtil {
 		return employee;
 	}
 
+	public static DtoEmployee dtoNewEmployee() {
+
+		DtoEmployee employee = new DtoEmployee();
+
+		employee.setEmployeeNumber(2L);
+		employee.setLastName("new_lastName");
+		employee.setFirstName("new_firstName");
+		employee.setExtension("new_extension");
+		employee.setEmail("new_email");
+		employee.setJobTitle("new_jobTitle");
+
+		return employee;
+	}
+
+	public static List<Employee> employees() {
+
+		List<Employee> employees = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			employees.add(employee());
+		}
+
+		return employees;
+	}
+
 	public static void assertEquals(Employee employee, DtoEmployee dtoEmployee) {
 
 		assertNotNull(employee, "Employee should not be null");
@@ -43,5 +71,16 @@ public class EmployeeUtil {
 
 		assertNotNull(office, "Office should not be null");
 		Assertions.assertEquals(office.getOfficeCode(), dtoEmployee.getOfficeCode());
+	}
+
+	public static void assertUpdated(final Employee original, DtoEmployee dtoEmployee, Employee employee) {
+
+		Assertions.assertEquals(original.getEmployeeNumber(), employee.getEmployeeNumber(),
+				"Employee number should match");
+		Assertions.assertEquals(dtoEmployee.getLastName(), employee.getLastName(), "Last name should match");
+		Assertions.assertEquals(dtoEmployee.getFirstName(), employee.getFirstName(), "First name should match");
+		Assertions.assertEquals(dtoEmployee.getExtension(), employee.getExtension(), "Extenstion should match");
+		Assertions.assertEquals(dtoEmployee.getEmail(), employee.getEmail(), "Email should match");
+		Assertions.assertEquals(dtoEmployee.getJobTitle(), employee.getJobTitle(), "Job title should match");
 	}
 }
