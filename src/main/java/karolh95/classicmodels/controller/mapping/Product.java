1510bcb9667a50f1203f2ec32534e2a401bd24d7
karolh95 <karolh95@live.com>
2020-07-31 14:52:09 +0200

    Created constants for Product Raports mappings

--- src/main/java/karolh95/classicmodels/controller/mapping/Product.java
+++ src/main/java/karolh95/classicmodels/controller/mapping/Product.java
@@ -3,4 +3,16 @@ package karolh95.classicmodels.controller.mapping;
 import karolh95.classicmodels.controller.mapping.Mappings.BasicMapping;
 
 public final class Product extends BasicMapping {
+
+	public static final String RAPORT = Mappings.PRODUCT + "/raport";
+
+	public static final class Raport {
+
+		public static final String BY_PRICE_BETWEEN = "price/between/{low}/{high}";
+		public static final String BY_PRICE_NOT_BETWEEN = "price/notBetween/{low}/{high}";
+
+		public static final String DESCRIPTION = "description";
+
+		public static final String DETAILS_BY_PRODUCTCODE = "details/productCode/{productCode}";
+	}
 }
