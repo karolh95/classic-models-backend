bf29b0e2cbc53c3aff168bc4064dd1f742dc811b
karolh95 <karolh95@live.com>
2020-07-31 14:34:44 +0200

    Created constants for Employee Raports mappings

--- src/main/java/karolh95/classicmodels/controller/mapping/Employee.java
+++ src/main/java/karolh95/classicmodels/controller/mapping/Employee.java
@@ -3,4 +3,24 @@ package karolh95.classicmodels.controller.mapping;
 import karolh95.classicmodels.controller.mapping.Mappings.BasicMapping;
 
 public final class Employee extends BasicMapping {
+
+	public static final String RAPORT = Mappings.EMPLOYEE + "/raport";
+
+	public static final class Raport {
+
+		public static final String SUMMARY = "summary";
+
+		public static final String BY_JOB_OFFICE = "jobTitle/{jobTitle}/officeCode/{officeCode}";
+
+		public static final String OFFICECODE_BETWEEN = "officeCode/{low}/{high}";
+
+		public static final String LASTNAME = "lastName/{lastName}";
+
+		public static final String CUSTOMER_PAYMENTS = "customer/payments";
+
+		public static final String WITH_CUSTOMER = "customer/true";
+		public static final String WITHOUT_CUSTOMER = "customer/false";
+
+		public static final String WITH_REPORTSTO = "reportsTo";
+	}
 }
