580a8cddb8f208cb5359ba8e2e8d69c5a7159924
karolh95 <karolh95@live.com>
2020-07-31 15:27:11 +0200

    Created constants for Order Raports mappings

--- src/main/java/karolh95/classicmodels/controller/mapping/Order.java
+++ src/main/java/karolh95/classicmodels/controller/mapping/Order.java
@@ -6,4 +6,19 @@ public final class Order extends BasicMapping {
 
 	public static final String PAYMENT_GENERATE = "payment/generate";
 	public static final String PAYMENT_MAKE = "payment/make";
+
+	public static final String RAPORT = Mappings.ORDER + "/raport";
+
+	public static final class Raport {
+
+		public static final String SUMMARY = "summary";
+		public static final String STATUS = "status";
+		public static final String TOTAL_GREATER_THAN = "total/greater/{total}";
+		public static final String REQUIRED_BETWEEN = "required/between/{from}/{to}";
+		public static final String REQUIRED_NOTBETWEEN = "required/notBetween/{from}/{to}";
+		public static final String TOTAL = "total";
+		public static final String PRODUCT_PRICE = "product/price";
+		public static final String CUSTOMER_PRICE = "customer/price";
+		public static final String BY_ORDERNUMBER = "orderNumber/{orderNumber}";
+	}
 }
