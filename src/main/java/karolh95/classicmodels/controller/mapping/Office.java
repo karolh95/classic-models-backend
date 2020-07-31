73c5059212d07225edad04903a4647bb5819ed21
karolh95 <karolh95@live.com>
2020-07-31 14:38:19 +0200

    Created constants for Office Raports mappings

--- src/main/java/karolh95/classicmodels/controller/mapping/Office.java
+++ src/main/java/karolh95/classicmodels/controller/mapping/Office.java
@@ -3,4 +3,12 @@ package karolh95.classicmodels.controller.mapping;
 import karolh95.classicmodels.controller.mapping.Mappings.BasicMapping;
 
 public final class Office extends BasicMapping {
+
+	public static final String RAPORT = Mappings.OFFICE + "/raport";
+
+	public static final class Raport {
+
+		public static final String COUNTRIES_IN = "countries/in/{country1}/{country2}";
+		public static final String COUNTRIES_NOT_IN = "countries/notIn/{country1}/{country2}";
+	}
 }
